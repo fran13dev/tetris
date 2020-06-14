62304ae6d57c9dc31306db65d09d69850fa17dae
Francois de Villiers <fran13dev@gmail.com>
2020-06-14 16:10:19 +0200

    Title change

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React, { useEffect } from 'react';
-import Tetris from './components/Tetris';
-import './App.css';
+import React, { useEffect } from 'react'
+import Tetris from './components/Tetris'
+import './App.css'
 
 const App = () => {
+	useEffect(() => {
+		// Update the document title using the browser API
+		document.title = 'Tetris - Fran13Dev'
+	})
 
-    useEffect(() => {
-        // Update the document title using the browser API
-        document.title = 'Tetris in Reactjs'
-    });
-
-    return (
-        <div className='app'>
-            <Tetris />
-        </div>
-    )
+	return (
+		<div className='app'>
+			<Tetris />
+		</div>
+	)
 }
 
 export default App
